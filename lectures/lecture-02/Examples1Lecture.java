d53e396b6ed40e021e520ac115b541c97c7264a2
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-08-02 16:07:20 -0700

    Lecture 02

---
+++ lectures/lecture-02/Examples1Lecture.java
@@ -0,0 +1,10 @@
+class Examples1Lecture {
+  int theNumberFive = 2 + 3;
+  int theNumberTwo = 6 / 3;
+  int theNumberTwelve = 6 * 2;
+
+  int x = (2 + 9) * 3;
+  int y1 = 10 / 3;
+  int y2 = 11 / 3;
+
+}
---
+++ lectures/lecture-02/Examples3.java
@@ -0,0 +1,10 @@
+class Examples3 {
+    int numberOfStaff = 14;         //14 - integer literal, integer value
+    String name = "Greg Miranda";   //"Greg Miranda" - string literal, string value
+
+    String className = "CSE11";
+    String className1 = "11";
+    //String className2 = 11;   //type error
+    int klassName = 11;
+    //int klassName1 = "11";    //type error
+}
