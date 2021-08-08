e502667b297e444c41b0d707c0a1dd70cf95251f
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-08-08 15:48:40 -0700

    Lecture 05

---
+++ lectures/lecture-05/ExamplesLec.java
@@ -0,0 +1,21 @@
+class Point {
+    int x;
+    int y;
+    Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    // Write a method called that takes another point
+    // and returns a new point with the two x coordinates added
+    // and the two y coordinates added as its x and y field
+
+}
+
+class ExamplesLec {
+  //Point
+  Point p1 = new Point(8, 16);
+  Point p2 = new Point(3, 4);
+
+
+}
---
+++ lectures/lecture-05/example1/ExamplesRegion.java
@@ -0,0 +1,34 @@
+/*
+class Point {
+    int x;
+    int y;
+    Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) 
+            + Math.pow(this.y - other.y, 2));
+      }
+  }
+
+  class CircRegion {
+    Point center;
+    int radius;
+    CircRegion(Point center, int radius) {
+      this.center = center;
+      this.radius = radius;
+    }
+
+    boolean contains(Point p) {
+        return this.center.distance(p) < this.radius;
+      }
+  }
+
+class ExamplesRegion {
+    CircRegion c1 = new CircRegion(new Point(200, 50), 10);
+    Point circleTest1 = new Point(209, 50);
+    boolean contains1 = this.c1.contains(this.circleTest1);
+}
+*/
