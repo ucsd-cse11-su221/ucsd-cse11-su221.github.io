4a1d681abdba47f6aca403b506358fd7f3f2f303
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-08-16 12:53:27 -0700

    Lecture 09 updates

--- lectures/lecture-09/ArrayExamples.java
+++ lectures/lecture-09/ArrayExamples.java
@@ -1,3 +1,37 @@
-class ArrayExamples {
+import tester.*;
 
+class ArrayExamples {
+    //int[] nums = { 4, 2, 7 };
+
+    //int firstNumber = this.nums[0];
+    //int secondNumber = this.nums[1];
+    //int thirdNumnber = this.nums[2];
+    //int fourthNumber = this.nums[3];  //out of bounds
+
+    String[] strs = { "a", "b", "c", "y" };
+    //String[] nAndS = { 2, "b", "z" };     //int cannot be converted to a string
+
+    int countStrs = strs.length;
+
+
+    int[] nums = { 4, 2, 7 };
+    int[] nums2 = nums;
+    int[] emptyNums = { };
+    
+    //Return true if the array has no elements, false otherwise
+    boolean isEmpty(int[] array) {
+        // if (array.length > 0) {
+        //     return false;
+        // }
+        // else {
+        //     return true;
+        // }
+
+        return array.length == 0;
+    }
+
+    boolean testEmpty(Tester t) {
+        return t.checkExpect(this.isEmpty(nums), false) &&
+               t.checkExpect(this.isEmpty(emptyNums), true);
+    }
 }
--- lectures/lecture-09/MainExample.java
+++ lectures/lecture-09/MainExample.java
@@ -1,3 +1,21 @@
 class MainExample {
 
+    int x = 5;
+
+    static int sum(int n, int m) {
+        //int y = this.x;   //no this - static method
+        return n + m;
+    }
+
+    public static void main(String[] args) {
+        //System.out.println("Hello");
+        //System.out.println("CSE 11!");
+
+        //System.out.println(MainExample.sum(6, 7));
+
+        String s1 = args[0];
+        System.out.println(s1);
+
+    }
+
 }
