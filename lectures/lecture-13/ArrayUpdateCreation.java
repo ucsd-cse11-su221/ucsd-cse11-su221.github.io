680bf16f7c09e662f3c9237f6164ec29d8d2ec1c
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-08-23 12:27:06 -0700

    Lecture 13 updates

--- lectures/lecture-13/ArrayUpdateCreation.java
+++ lectures/lecture-13/ArrayUpdateCreation.java
@@ -1,5 +1,36 @@
+import jdk.javadoc.internal.tool.Start;
 import tester.*;
 
 class ArrayUpdateCreation {
+    int[] a = new int[7];
+    boolean[] b = new boolean[3];
+    double[] d = new double[2];
+    String[] strs = new String[3];
 
+    String s0 = strs[0];
+    String s1 = strs[1];
+    //int lengthofS = s.length();
+
+    {
+        this.strs[1] = "abc";
+    }
+
+    String s1Again = strs[1];
+
+    // Returns a new array of numbers
+    // from start (inclusive) to stop (exclusive)
+    int[] range(int start, int stop) {
+        int size = stop - start;
+        int[] nums = new int[size];
+        for (int index = 0; index < size; index += 1) {
+            nums[index] = index + start;
+        }
+        return nums;
+    }
+
+    int[] range1 = range(20, 30);
+    //              0   1   2   3   4   5   6   7   8   9
+    // look like { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 }
+    int[] range2 = range(0, 5);
+    // look like { 0, 1, 2, 3, 4 }
 }
--- lectures/lecture-13/PointExamples.java
+++ lectures/lecture-13/PointExamples.java
@@ -21,8 +21,46 @@ class Point {
   // the point in the array pts that is the *closest* (according to distance) to
   // this Point
 
+  // Write a method that takes an array of Points pts and an integer dist
+  // and returns a new array of all points within dist of this point
+  // from that array
+  Point[] allWithin(Point[] pts, int dist) {
+    int numberWithin = 0;
+    for (Point p: pts) {
+      if (this.distance(p) < dist) {
+        numberWithin += 1;
+      }
+    }
+
+    Point[] toReturn = new Point[numberWithin];
+    int index = 0;
+
+    for (Point p: pts) {
+      if (this.distance(p) < dist) {
+          toReturn[index] = p;     //array update expression
+          index += 1;
+      }
+    }
+
+    return toReturn;
+  }
 }
 
 class PointExamples {
 
+  Point p1 = new Point(4, 5);
+  Point p2 = new Point(10, 10);
+  Point p3 = new Point(100, 100);
+  Point p4 = new Point(0, 0);
+
+  Point[] allButP1 = { p2, p3, p4 };
+  Point[] allButP1P3 = { p2, p4 };
+  Point[] empty = { };
+
+  Point[] result1 = p1.allWithin(allButP1, 20);
+
+  void testAllWithin(Tester t) {
+    t.checkExpect(p1.allWithin(allButP1, 20), allButP1P3);
+    t.checkExpect(p1.allWithin(allButP1, 3), empty);
+  }
 }
