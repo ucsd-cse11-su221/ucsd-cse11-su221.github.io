e093196a935c2493724063f84dd86f35e4f5b386
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-08-29 12:31:24 -0700

    Lecture 17

---
+++ lectures/lecture-17/ToString.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Pair {
+    int a, b;
+    Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
+
+class ToString {
+    public static void main(String[] args) {
+        Integer i = 40;
+        Integer[] nums = { 4, 5, 6 };
+
+        List<Integer> ints = Arrays.asList(1, 2, 3);
+
+    }
+}
